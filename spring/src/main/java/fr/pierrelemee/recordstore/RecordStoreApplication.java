92974e98128ad71988d8fd9bc71cd3d5f3a8296c
Pierre LEMEE <pierre@pierrelemee.fr>
2024-07-26 13:02:03 +0200

    Basic templated page with Thymeleaf

--- spring/src/main/java/fr/pierrelemee/recordstore/RecordStoreApplication.java
+++ spring/src/main/java/fr/pierrelemee/recordstore/RecordStoreApplication.java
@@ -5,18 +5,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
 public class RecordStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecordStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecordStoreApplication.class, args);
+    }
 
-	@GetMapping("/hello")
+    @GetMapping(value = "/", name = "index")
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("index");
+        mav.addObject("title", "Bienvenue");
+        return mav;
+    }
+
+
+    @GetMapping(value = "/hello", name = "hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Bien le bonjour %s!", name);
+        return String.format("Bien le bonjour %s!", name);
     }
 
 }
